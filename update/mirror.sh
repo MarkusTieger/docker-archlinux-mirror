51d2322942713b59d8f7492e551a79957169dde7
MarkusTieger <77546847+MarkusTieger@users.noreply.github.com>
2023-05-19 11:47:34 +0200

    Update mirror.sh

--- update/mirror.sh
+++ update/mirror.sh
@@ -4,10 +4,12 @@
 RSYNC=/usr/bin/rsync
 
 #MIRROR=rsync://mirrors.uk2.net/archlinux/
-MIRROR=rsync://mirror.de.leaseweb.net/archlinux/
+#MIRROR=rsync://mirror.de.leaseweb.net/archlinux/
 #MIRROR=rsync://ftp5.gwdg.de/pub/linux/archlinux/
 #MIRROR=rsync://rsync.gtlib.gatech.edu/archlinux/
 #MIRROR=rsync://mirror.rit.edu/archlinux/
+
+MIRROR=rsync://os.codefionn.eu/archlinux/
 
 OPTIONS=-"rtlvH --delete-after --delay-updates --safe-links --max-delete=3000"
 
